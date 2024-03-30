081d4795a3159549db9a8a251a2cc0a93b4183cc
lim10dev <165482550+aeuludag@users.noreply.github.com>
2024-03-30 19:10:19 +0300

    Created a basic homepage
    - Added a basic scrolling sparkles background
    - Added ProjectLink and ProjectLinks components
    - Added Poppins as a font
    
    To Do:
    - Find a better way of importing images to ProjectLink(s)

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProjectLinks from '../ProjectLinks/ProjectLinks'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='title'>Ahmet Emir's Projects</h1>
+      <ProjectLinks></ProjectLinks>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/ProjectLinks/ProjectLinks.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import LogicImage from "../../assets/Icons/logic.png";
+import "./ProjectLinks.css"
+
+function ProjectLinks() {
+
+    const projects = [
+        { projectName: "Logic", route: "logic", description: "A tool to calculate boolean logic." },
+        { projectName: "Unlogic", route: "unlogic", description: "A tool not to calculate boolean logic." },
+    ]
+
+    return <>
+        <div>
+            <ul>
+            {projects.map(({ projectName, route, description }) => {
+                return <li key={route}>
+                        <ProjectLink projectName={projectName} route={route} description={description}></ProjectLink>
+                    </li>
+            })}
+            </ul>
+        </div>
+    </>
+}
+
+function ProjectLink(props) {
+    return <div className="project-link-container">
+        <Link className={`project-link link-to-${props.route}`} to={props.route}>
+            <img className="project-image" src={LogicImage}></img>
+            {props.projectName}
+        </Link>
+        <p className={`project-description desc-to-${props.route}`}>{props.description}</p>
+    </div>
+}
+
+export default ProjectLinks;
