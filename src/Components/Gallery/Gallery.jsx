ddea930a8909cc31ccd0f6c52f1fab7dac00b517
lim10dev <165482550+aeuludag@users.noreply.github.com>
2024-04-20 23:06:39 +0300

    Removed Design Inspection
    - Removed desing inspection for now
    - Changed the designs in gallery for ones with smaller resolutions

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,19 +1,14 @@
-import { Link } from "react-router-dom";
-import closeImage from "../../assets/close.svg"
 import openInNewImage from "../../assets/open_in_new.svg"
 import designs from "../../designs"
 import "./Gallery.css"
-import { useState } from "react";
 
 
 function Gallery() {
-    const [currentDesign, setCurrentDesign] = useState(false)
-
     let designElements = []
 
     for (let i = 0; i < designs.length; i++) {
         let design = designs[i]
-        let element = <GalleryDesign key={design.id} design={design} set={setCurrentDesign} />;
+        let element = <GalleryDesign key={design.id} design={design} />;
         designElements.push(element);
     }
 
@@ -24,7 +19,6 @@ function Gallery() {
             <div className="gallery-items">
                 {designElements}
             </div>
-            {currentDesign && <ZoomedDesign design={currentDesign} set={setCurrentDesign} />}
         </>)
 }
 
@@ -34,37 +28,16 @@ function GalleryDesign(props) {
     const description = props.design.description;
 
     return (
-        <div className="gallery-design" onClick={() => props.set(props.design)}>
-            <img className="design-image" src={`/designs/${id}.png`} alt={description} />
+        <div className="gallery-design" tabIndex={"0"}>
+            <img className="design-image" src={`/designs/lowres/${id}.png`} alt={description} />
             <div className="design-details">
                 <h1 className="design-title">
                     {title}
                 </h1>
                 <p className="design-description">{description}</p>
             </div>
-        </div>
-    )
-}
-
-function ZoomedDesign(props) {
-    const id = props.design.id;
-    const title = props.design.title;
-    const description = props.design.description;
-
-    return (
-        <div className="zoomed-design-wrapper">
-            <div className="zoomed-design">
-                <div className="zoomed-design-buttons">
-                    <a className="image-link" target="blank" href={`/designs/${id}.png`}><img src={openInNewImage} /></a>
-                    <button className="close-button" onClick={() => props.set(null)}><img src={closeImage} /></button>
-                </div>
-                <img className="zoomed-image" src={`/designs/${id}.png`} alt={description} />
-                <div className="zoomed-details">
-                    <h1 className="zoomed-title">
-                        {title}
-                    </h1>
-                    <p className="zoomed-description">{description}</p>
-                </div>
+            <div className="design-buttons">
+                <a className="design-button image-link" target="_blank" href={`/designs/${id}.png`}><img src={openInNewImage} /></a>
             </div>
         </div>
     )
